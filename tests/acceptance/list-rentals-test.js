9108053555693a9542f194d6099e95822cf98c57
jlaw <ordinarycryptid@gmail.com>
2018-12-10 12:56:02 -0500

    first commit

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,40 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', function(assert){
+
+  });
+
+  test('should link to information about the company', function(assert) {
+
+  });
+
+  test('should link to contact information.', function(assert) {
+
+  });
+
+  test('should list available rentals.', function(assert){
+
+  });
+  
+  test('should fliter the list of rentals by city.', function(assert){
+
+  });
+
+  test('should show details for a selected rental', function(assert) {
+    
+  })
+
+  // // test('visiting /list-rentals', async function(assert) {
+  // test('visiting /', async function(assert) {
+  //   // await visit('/list-rentals');
+  //   await visit('/');
+
+  //   // assert.equal(currentURL(), '/list-rentals');
+  //   assert.equal(currentURL(), '/');
+  // });
+});
